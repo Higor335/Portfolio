1c29deee48a7500f8c01a867c33df0822d594df6
higor335 <higor-lopo@hotmail.com>
2024-03-03 19:46:52 -0400

    feat: :art: Corrige estrutura de arquivos

---
+++ Portfolio/src/components/Footer.tsx
@@ -0,0 +1,41 @@
+function Footer(){
+    return(
+        <>
+            <footer>
+                <h3>CONTATOS</h3>
+                <br /><hr /><br />
+
+                
+                <div className="redeSociais">
+                    
+                    <a href="">
+                        <div>
+                            <img src="" alt="" />
+                            <br /><p>Linkedin</p>
+                            <br /><p>Higor Vital Lopo</p>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div>
+                            <img src="" alt="" />
+                            <br /><p>Itch.io</p>
+                            <br /><p>@Higor-vl</p>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div>
+                            <img src="" alt="" />
+                            <br /><p>GitHub</p>
+                            <br /><p>@Higor335</p>
+                        </div>
+                    </a>
+
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
---
+++ Portfolio/src/components/Header.tsx
@@ -0,0 +1,17 @@
+function Header(){
+    return(
+        <>
+            <header>
+                <img src="" alt="logo" />
+                <div>
+                    <button>Inicio</button>
+                    <button>Sobre</button>
+                    <button>Projetos</button>
+                    <button>Contatos</button>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header;
