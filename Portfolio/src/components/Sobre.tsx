fe7d585d2b9ebec24384e9a8d1d84bf4aa7da4f9
Higor V.L. <higor-lopo@hotmail.com>
2024-03-08 01:13:11 -0400

    feat: :sparkles: item linguagens e tecnologias na aba sobre

--- Portfolio/src/components/Sobre.tsx
+++ Portfolio/src/components/Sobre.tsx
@@ -9,27 +9,29 @@ export default function Sobre(){
                 <div className="bloco">
                     <img src="src\assets\eu.jpeg" alt="minha foto" className="minhaFoto"/>
 
-                    <div className="blocoTexto">
+                    <div className="info">
                         <h3>Olá, Tudo Bem?<br /> Eu sou o Higor Vital Lopo 👋</h3>
                         <p>
                             Olá, meu nome é Higor e tenho 22 anos. Sou formado em <strong>Sistemas de Informação</strong> aficionado por <strong className="strong-light-blue">Desenvolvimento Web</strong>, <strong className="strong-green">Sistemas</strong> e aspirante a <strong className="strong-dark-purple">Desenvolvedor de Jogos</strong> 👾.
                             Após participar de competições de robótica em tempos de escola e ser recompensado com o prêmio de melhor programação do estado resolvi investir e me dedicar ao que realmente gosto...programar!
                         </p>
+                        <p className="linguagensTitulo">Linguagens e Tecnologias</p>
+                        <fieldset className="linguagens">
+                            <img src="src\assets\html5.png" alt="" />
+                            <img src="src\assets\css3.png" />
+                            <img src="src\assets\sass.png" alt="" />
+                            <img src="src\assets\js.png" alt="" />
+                            <img src="src\assets\typescript.png" alt="" />
+                            <img src="src\assets\react.png" alt="" />
+                            <img src="src\assets\wordpress.png" alt="" />
+                            <img src="src\assets\java.png" alt="" />
+                            <img src="src\assets\python.png" alt="" />
+                            <img src="src\assets\mysql.png" alt="" />
+                        </fieldset>
                     </div>
-
                 </div>
 
                 <br />
-
-                <div className="linguagens">
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                </div>
             </div>
     )
 }
