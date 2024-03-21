7cdf61d5f5be4fd6377f0b42a4b69c105656e19f
higor335 <higor-lopo@hotmail.com>
2024-03-21 00:42:30 -0400

    feat: :sparkles: projetos

--- Portfolio/src/components/Projetos.tsx
+++ Portfolio/src/components/Projetos.tsx
@@ -8,27 +8,27 @@ export default function Projetos(){
 
             <div className="conteudo">
                 <div className="blocos">
-                    <img src="src\assets\Projetos\SpaceKiller2D.png" alt="spacekiller" />
-                    <h3>Space Killer 2D</h3><hr />
-                    <p>Jogo 2D feito em unity</p>
+                    <img src="src\assets\Projetos\BooksCorumba.png" alt="BooksCorumba" />
+                    <h3>BOOKS CORUMBÁ</h3><hr />
+                    <p>Site Completo simulando uma loja de livros!</p>
+                </div>
+
+                <div className="blocos">
+                    <img src="src\assets\Projetos\Receitas.png" alt="Receitas" />
+                    <h3>REPOSITÓRIO DE RECEITAS</h3><hr />
+                    <p>Repositório web com mais de 235 receitas!</p>
+                </div>
+
+                <div className="blocos">
+                    <img src="src\assets\Projetos\Sputfy.png" alt="Sputfy" />
+                    <h3>SPUTFY</h3><hr />
+                    <p>Projeto de player de músicas</p>
                 </div>
 
                 <div className="blocos">
                     <img src="src\assets\Projetos\SpaceKiller2D.png" alt="spacekiller" />
-                    <h3>Space Killer 2D</h3><hr />
-                    <p>Jogo 2D feito em unity</p>
-                </div>
-
-                <div className="blocos">
-                    <img src="src\assets\Projetos\SpaceKiller2D.png" alt="spacekiller" />
-                    <h3>Space Killer 2D</h3><hr />
-                    <p>Jogo 2D feito em unity</p>
-                </div>
-
-                <div className="blocos">
-                    <img src="src\assets\Projetos\SpaceKiller2D.png" alt="spacekiller" />
-                    <h3>Space Killer 2D</h3><hr />
-                    <p>Jogo 2D feito em unity</p>
+                    <h3>SPACE KILLER 2D</h3><hr />
+                    <p>Jogo 2D feito em unity!</p>
                 </div>
 
                 <div className="blocos">
