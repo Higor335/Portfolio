792878bc34c668cecdd24b303d788d3fcafbeeb5
Higor V.L. <higor-lopo@hotmail.com>
2024-03-24 03:31:49 -0400

    feat: :sparkles: modularização dos blocos de projetos

--- Portfolio/src/components/Projetos.tsx
+++ Portfolio/src/components/Projetos.tsx
@@ -12,7 +12,8 @@ export default function Projetos(){
                 <ProjetoBloco 
                     imagem={"src/assets/Projetos/BooksCorumba.png"}
                     titulo={"BOOKS CORUMBÁ"}
-                    descricao={"Site Completo simulando uma loja de livros!"} 
+                    descricao={"Loja digital de livros, com tecnologias de front e back-end!"} 
+                    detalhes={'Site feito mesclando conhecimentos em FRONT-END e BACK-END, sendo o Front feito em React+Vite, Typescript, HTML, SASS. Enquanto o Back-end foi feito utilizando Node-JS e Javascript, junto ao mongoDB. Site sem fins lucrativos.<br> Todos os direitos autorais dos livros utilizados pertencem aos seus respectivos proprietários.'}
                     link={"https://higor335.github.io/BooksCorumba/"}
                 />
                
@@ -20,6 +21,7 @@ export default function Projetos(){
                     imagem={"src/assets/Projetos/Receitas.png"}
                     titulo={"REPOSITÓRIO DE RECEITAS"}
                     descricao={"Repositório web com mais de 235 receitas!"} 
+                    detalhes=""
                     link={"https://higor335.github.io/REpoCIPES/"}
                 />
 
@@ -27,6 +29,7 @@ export default function Projetos(){
                     imagem={"src/assets/Projetos/Sputfy.png"}
                     titulo={"SPUTFY"}
                     descricao={"Projeto de player de músicas!"} 
+                    detalhes=""
                     link={"https://higor335.github.io/Site-Teste_SPUTFY/"}
                 />
 
@@ -34,6 +37,7 @@ export default function Projetos(){
                     imagem={"src/assets/Projetos/SpaceKiller2D.png"}
                     titulo={"SPACE KILLER 2D"}
                     descricao={"Jogo 2D feito em unity para desktop!"} 
+                    detalhes=""
                     link={"https://itch.io/embed-upload/9315862?color=000044"}
                 />
 
@@ -41,6 +45,7 @@ export default function Projetos(){
                     imagem={"src/assets/Projetos/jogoDaForca1.png"}
                     titulo={"JOGO DA FORCA"}
                     descricao={"Jogo para celular feito no Android Studio!"} 
+                    detalhes=""
                     link={""}
                 />
 
@@ -48,6 +53,7 @@ export default function Projetos(){
                     imagem={"src/assets/Projetos/EnigmaDungeon.png"}
                     titulo={"ENIGMA DUNGEON"}
                     descricao={"Jogo 3D feito em unity para desktop!"} 
+                    detalhes=""
                     link={""}
                 />
             </div>
--- Portfolio/src/components/project objects/ProjetoBloco.tsx
+++ Portfolio/src/components/project objects/ProjetoBloco.tsx
@@ -4,17 +4,39 @@ interface Bloco{
     imagem:string, 
     titulo:string,
     descricao:string,
+    detalhes:string,
     link:string
 }
 
-function ProjetoBloco({imagem, titulo, descricao, link}:Bloco){
+
+function toggleContent(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const element = event.currentTarget as HTMLDivElement;
+    element.classList.toggle('flipped');
+    const content = element.querySelector('.conteudoOculto') as HTMLElement;
+    content.classList.toggle('hidden');
+}
+
+
+
+function ProjetoBloco({imagem, titulo, descricao, detalhes, link}:Bloco){
+    
     return(        
-        <div className="blocos">
+        <div className="blocos" onClick={toggleContent}>
+
+            <img className="capa" src={imagem} alt={titulo} />
+            <h3>{titulo}</h3><hr />
+            <p className="desc">{descricao}</p>
+            <a className="botao" href={link} target="_blank"><button>Ver Projeto</button></a>
+            
+            <div className="conteudoOculto">
                 <img src={imagem} alt={titulo} />
-                <h3>{titulo}</h3><hr />
-                <p>{descricao}</p>
+                <p>{detalhes}</p>
                 <a href={link} target="_blank"><button>Ver Projeto</button></a>
-        </div>        
+            </div>
+        </div>
+
+
+    
     )
 }
 
