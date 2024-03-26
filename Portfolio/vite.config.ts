f68d9a323a00ad6c57318215769181cb13966017
higor335 <higor-lopo@hotmail.com>
2024-03-26 00:29:45 -0400

    feat: :rocket:

--- Portfolio/vite.config.ts
+++ Portfolio/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import sassPlugin from 'vite-plugin-sass';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react(),sassPlugin()],
+  base:"/Portfolio/",
+  plugins: [react()],
 })
--- README.md
+++
@@ -1,1 +0,0 @@
-Meu portfólio com o compilado dos meus principais projetos desenvolvidos no decorrer dos ultimos anos do curso de sistemas de informação!
